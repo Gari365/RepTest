ef6e38bab17e8681e8594796c061dbded5b1107e
Gariii <thewallofgarrisons@gmail.com>
2024-08-31 10:42:36 +0700

    Name Input

--- GariTestFile.cpp
+++ GariTestFile.cpp
@@ -3,8 +3,13 @@ using namespace std;
 
 int main () {
 
-
+  string Name;
+  
   cout<<"Hello, World!"<<endl;
+  cout<<"Masukkan nama : ";
+  cin>>Name;
+
+  cout<<endl<<"Name : "<<Name;
   
 return 0;
 }
